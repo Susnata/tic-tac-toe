245fd51cc12ea3d29bce6c74380a99abb63fc3c9
Susnata Dube <susnata03@gmail.com>
2020-06-02 21:13:34 +0530

    Tic-Tac-Toe v2.0 CSS changes

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-const boxElements = document.getElementsByClassName("box");
 const gameBoardEl = document.getElementById("game-board");
 const gameStatusEl = document.getElementById("game-status");
 let count = 0, turn = "O"; let occupiedCellList = [];
 const turnX = "X", turnO = "O";
-//let firstPlayer;
 
 function playBtnClick() {
-  // console.log(this);
   this.classList.add("hide");
   gameStatusEl.textContent = "Game started";
-  // for (let el of boxElements) {
-  // console.log(el);
   gameBoardEl.addEventListener("mouseover", handleMouseOver);
-
   gameBoardEl.addEventListener("mouseout", handleMouseOut);
-
   gameBoardEl.addEventListener("click", handleClick);
-  // }
 }
 
 function handleMouseOver(e) {
@@ -30,9 +22,7 @@ function handleMouseOver(e) {
       targetElement.textContent = turnO;
     }
     e.stopPropagation();
-
   }
-
 }
 
 function handleMouseOut(e) {
@@ -89,25 +79,3 @@ playBtnEl.addEventListener("click", playBtnClick);
 
 
 
-    // if (turn >= 5) {
-    //   switch (true) {
-    //     case turnSequence[0] === turnSequence[1] && turnSequence[1] === turnSequence[2]:
-    //     case turnSequence[0] === turnSequence[3] && turnSequence[3] === turnSequence[6]:
-    //     case turnSequence[0] === turnSequence[4] && turnSequence[4] === turnSequence[8]:
-    //       gameStatusEl.textContent = "Player " + turnSequence[0] + " Wins";
-    //       break;
-    //     case turnSequence[1] === turnSequence[4] && turnSequence[4] === turnSequence[7]:
-    //       gameStatusEl.textContent = "Player " + turnSequence[1] + " Wins";
-    //       break;
-    //     case turnSequence[2] === turnSequence[4] && turnSequence[4] === turnSequence[6]:
-    //     case turnSequence[2] === turnSequence[5] && turnSequence[5] === turnSequence[8]:
-    //       gameStatusEl.textContent = "Player " + turnSequence[2] + " Wins";
-    //       break;
-    //     case turnSequence[3] === turnSequence[4] && turnSequence[4] === turnSequence[5]:
-    //       gameStatusEl.textContent = "Player " + turnSequence[3] + " Wins";
-    //       break;
-    //     case turnSequence[6] === turnSequence[7] && turnSequence[7] === turnSequence[8]:
-    //       gameStatusEl.textContent = "Player " + turnSequence[6] + " Wins";
-    //   }
-
-    // }
